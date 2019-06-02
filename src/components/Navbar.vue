2726ff694c37ba0d2ddec7d48545dec67630df9b
winnee0solta <winneecreztha@gmail.com>
2019-06-02 22:04:18 +0545

    added tooltip for add icon on navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,43 +1,49 @@
 <template>
   <v-toolbar color="primary white--text">
-    <v-toolbar-title class="nav-title" @click="goHome" >Smoothies ❤️</v-toolbar-title> 
-    <v-spacer></v-spacer> 
-      <v-btn
-            fab
-            color="secondary white--text"
-            bottom
-            right
-            absolute 
-            @click="createNew()"
-          >
-            <v-icon>add</v-icon>
-          </v-btn> 
+    <v-toolbar-title class="nav-title" @click="goHome">Smoothies ❤️</v-toolbar-title>
+    <v-spacer></v-spacer>
+
+    <v-tooltip left>
+      <template v-slot:activator="{ on }">
+        <v-btn
+          fab
+          color="secondary white--text"
+          bottom
+          right
+          absolute
+          @click="createNew()"
+          v-on="on"
+        >
+          <v-icon>add</v-icon>
+        </v-btn>
+      </template>
+      <span>Add Smoothie</span>
+    </v-tooltip>
   </v-toolbar>
 </template>
 
 <script>
-export default { 
-    name : 'Navbar', 
-    methods: {
-        createNew(){
-            console.log('create new hit!');
-            this.$router.push('/add-smoothie')
-        },
-        goHome(){
-            this.$router.push({name:'home'})
-        }
+export default {
+  name: "Navbar",
+  methods: {
+    createNew() {
+      console.log("create new hit!");
+      this.$router.push("/add-smoothie");
     },
-}
+    goHome() {
+      this.$router.push({ name: "home" });
+    }
+  }
+};
 </script>
  
 
 <style lang="scss" scoped>
-@import url('https://fonts.googleapis.com/css?family=Cookie&display=swap'); 
-  
-.nav-title{
-    font-family: 'Cookie', cursive;
-    font-size: 35px;
-    cursor: pointer;
-}
+@import url("https://fonts.googleapis.com/css?family=Cookie&display=swap");
 
+.nav-title {
+  font-family: "Cookie", cursive;
+  font-size: 35px;
+  cursor: pointer;
+}
 </style>
